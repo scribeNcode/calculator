18954cbe971dffb476a22b6ab5aea9cdf8b2be25
scribeNcode <deyeye99@gmail.com>
2025-01-04 15:23:34 +0100

    create a function to add, subtract,multiply and divide two numbers

---
+++ script.js
@@ -0,0 +1,34 @@
+// create a  function to add numbers
+function addNum(number1, number2){
+let sum = number1 + number2
+console.log(sum);
+return sum
+}
+
+// create a  function to subtract numbers
+function subNum(number1, number2){
+    let sub = number1 - number2
+    console.log(sub);
+    return sub
+}
+
+// create a  function to multiply numbers
+function multiNum(number1, number2){
+let multi = number1 * number2;
+console.log(multi)
+return multi
+}
+
+// create a  function to divide numbers
+function divideNum(number1, number2){
+    let divide = number1 / number2;
+    console.log(divide)
+    return divide
+    }
+
+// call functions
+addNum(1,2);
+subNum(8,6);
+multiNum(4,2);
+divideNum(4,2)
+
